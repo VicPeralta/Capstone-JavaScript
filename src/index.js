53a1a00b374ef341a6dcefdb3a1f9fb903703613
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-08 19:29:26 +0100

    Add header feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './style.css';
+import logo from './assets/tunes4.png';
+const headerLogo = document.querySelector('.img');
+headerLogo.innerHTML = `<img class="Img" src="${logo}" alt="">`;
+// const Img = document.querySelector('.Img');
+// Img.style.width = '1rem';
