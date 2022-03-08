4918671d07b25dad0f443ba6d1d7855472ad81d8
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-08 19:49:09 +0100

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import './style.css';
 import logo from './assets/tunes4.png';
+
 const headerLogo = document.querySelector('.img');
 headerLogo.innerHTML = `<img class="Img" src="${logo}" alt="">`;
-// const Img = document.querySelector('.Img');
-// Img.style.width = '1rem';
--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-        
+
       },
-      
+
     ],
   },
 };
