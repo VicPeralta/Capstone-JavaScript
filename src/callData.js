96bc3834c9b212df6ac2ba8ea7cfa1ba5ab22039
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-03-12 10:33:40 -0600

    Fix linter errors

--- src/callData.js
+++
@@ -1,6 +0,0 @@
-function callData(data) {
-  console.log(data);
-}
-const script = document.createElement('script');
-script.src = 'https://itunes.apple.com/search?term=javascript&media=ebook&limit=48&country=US&callback=callData';
-document.body.append(script);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import './style.css';
 import logo from './assets/itunesW.png';
 import App from './app.js';
 
-function callData(data) {
-  console.log(data);
-}
-
 document.querySelector('header img').setAttribute('src', logo);
 
 const theApp = new App();
